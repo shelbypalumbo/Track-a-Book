5fbefaf46d6347fca50c65e08d4a991ddde87430
shelby palumbo <shelbypalumbo94@gmail.com>
2020-05-29 23:06:05 -0400

    adding readme

---
+++ client/build/precache-manifest.521eaa66f0fe483a9fcb509a5f9bca48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "55febf5fdc1c8383ff45",
+    "url": "/static/css/main.d9ca160d.chunk.css"
+  },
+  {
+    "revision": "55febf5fdc1c8383ff45",
+    "url": "/static/js/main.1695d5c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "99b1bb2e2d21942ddda9",
+    "url": "/static/css/2.c562a18e.chunk.css"
+  },
+  {
+    "revision": "99b1bb2e2d21942ddda9",
+    "url": "/static/js/2.4be84e3d.chunk.js"
+  },
+  {
+    "revision": "7a9e9f27ff577713ee3c377a9c4a4512",
+    "url": "/static/media/Track-a-Book.7a9e9f27.png"
+  },
+  {
+    "revision": "dbe36fddaffd022f551d72c1592426bc",
+    "url": "/index.html"
+  }
+];
